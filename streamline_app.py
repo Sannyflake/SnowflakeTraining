0efe6944435a8cd6680666db3e009e83f5e3d306
Sannyflake <135823900+Sannyflake@users.noreply.github.com>
2023-06-07 12:31:36 +0200

    Update streamline_app.py
    typo fix

--- streamline_app.py
+++ streamline_app.py
@@ -5,7 +5,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocekt Smoothie')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Bolied Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
